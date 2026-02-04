ea099417d6420f7a260463da24d0e0e92242d911
testauthosk <openclaw@users.noreply.github.com>
2026-02-04 22:22:35 +0100

    Revert "ui: increase vertical spacing in staff dashboard"
    This reverts commit 1b061623792cc115eb81b37608745f79b95fc313.

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -545,7 +545,7 @@ export default function StaffDashboard() {
       </header>
 
       {/* Content */}
-      <div className="p-4 space-y-6 pb-6">
+      <div className="p-4 space-y-4 pb-6">
         {/* Greeting */}
         <div>
           <h1 className="text-2xl font-bold">{greeting}!</h1>
