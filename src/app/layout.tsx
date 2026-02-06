4f8751826858f4b1bdc3de1086aacce30feb4974
testauthosk <openclaw@users.noreply.github.com>
2026-02-06 17:37:25 +0100

    Body bg-gray-50 to match calendar background in menu gaps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="uk" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
       >
         <ViewTransitions>
           <ThemeProvider
